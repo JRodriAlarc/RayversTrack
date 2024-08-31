8ada7e3c12839a44c2d158520d7b59b488e88607
JRodriAlarc <jona10131313@gmail.com>
2024-08-30 21:15:21 -0600

    Creación del Proyecto

---
+++ src/main/java/rayversTrack/RayversTrackApplication.java
@@ -0,0 +1,13 @@
+package rayversTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayversTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayversTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rayversTrack/RayversTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package rayversTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayversTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
